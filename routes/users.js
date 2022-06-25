3f6ab35daecf10105a090b28ff9bbc012db3857b
kris1803 <78277770+kris1803@users.noreply.github.com>
2022-06-25 19:02:17 +0200

    added tooltips and background image changed

--- routes/users.js
+++ routes/users.js
@@ -12,43 +12,46 @@ router.get('/', function (req, res) {
 
 router.post('/sign-up', async function (req, res) {
   // check if post data received
-  if (req.body.email && req.body.password && req.body.username && mailvalidator.validate(req.body.email) && req.body.password.length >= 6 && req.body.username.length >= 3) {
-    // check if user already exists
-    let username;
-    let sameEmail;
-    try {
+  if (!req.body.email || !req.body.password || !req.body.password || !mailvalidator.validate(req.body.email) || req.body.password.length < 6 || req.body.username.length < 3) {
+    res.render('login', { error: 'Please fill in all fields correctly.' });
+    return;
+  }
+
+  // if (req.body.email && req.body.password && req.body.username && mailvalidator.validate(req.body.email) && req.body.password.length >= 6 && req.body.username.length >= 3) {
+  // check if user already exists
+  let username;
+  let sameEmail;
+  try {
     username = await userModel.findOne({ username: req.body.username });
     sameEmail = await userModel.findOne({ email: req.body.email });
-    } catch (error) {
-      console.log(error);
-      res.render('login', { error: 'Something went wrong.' });
-      return;
-    }
-    if (sameEmail || username) {
-      res.render('login', { error: 'User already exists.' });
-      return;
-    }
-    const hash = await bcrypt.hash(req.body.password, 10);
-    // save informations from form in database
-    let newUser = new userModel({
-      username: req.body.username,
-      password: req.body.password,
-      email: req.body.email
-    });
-    newUser.save(function (err, user) {
-      if (err) {
-        console.log(err);
-        res.render('login', { error: 'Server error.' });
-        return;
-      }
-      // utilisateur a été enregistré
-      req.session.username = user.username;
-      req.session._id = user._id;
-      res.redirect('/weather');
-    })
-  } else {
-    res.render('login', { error: 'Invalid data. Password must be at least 6 char long.' });
+  } catch (error) {
+    console.log(error);
+    res.render('login', { error: 'Something went wrong.' });
+    return;
   }
+  if (sameEmail || username) {
+    res.render('login', { error: 'User already exists.' });
+    return;
+  }
+  const hash = await bcrypt.hash(req.body.password, 10);
+  // save informations from form in database
+  let newUser = new userModel({
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email
+  });
+  newUser.save(function (err, user) {
+    if (err) {
+      console.log(err);
+      res.render('login', { error: 'Server error.' });
+      return;
+    }
+    // utilisateur a été enregistré
+    req.session.username = user.username;
+    req.session._id = user._id;
+    res.redirect('/weather');
+  })
+
 });
 
 router.post('/sign-in', function (req, res, next) {
